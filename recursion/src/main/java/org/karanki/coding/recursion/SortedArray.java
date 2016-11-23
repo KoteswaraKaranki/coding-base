0dcff61ad8f1d313d4ced00bd6ab37013eedde10
Koti Karanki <koteswara.r.karanki@gmail.com>
2016-11-23 21:25:39 +0530

    Linked List ADT, Recursion algorithsm added

---
+++ recursion/src/main/java/org/karanki/coding/recursion/SortedArray.java
@@ -0,0 +1,43 @@
+package org.karanki.coding.recursion;
+
+import java.util.Scanner;
+
+public class SortedArray {
+
+	public static void main(String[] args) {
+		
+		go();
+
+	}
+	
+	
+	private static void go()
+	{
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		int []A = new int[n];
+
+		
+		for(int c = 0; c < n; c++)
+		{
+			 A[c] =  scan.nextInt();
+		}
+			
+		
+		System.out.println(ascSort(A, A.length));
+		
+		scan.close();
+		
+	}
+
+
+	private static boolean ascSort(int[] A, int n) {
+		
+		if(n == 1)
+			return true;
+		
+		return A[n - 1] < A[n - 2] ? false : ascSort(A, n - 1);
+	}
+
+}
